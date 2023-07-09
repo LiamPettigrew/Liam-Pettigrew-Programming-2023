02a527ce2cb4232a731f81c30f104a24dd8583a8
LiamPettigrew <80730203+LiamPettigrew@users.noreply.github.com>
2023-07-09 06:26:01 +0000

    COMMIT 6 - Added the Javascript functionality to the trial quiz box in the automobile level 1 section.

---
+++ script.js
@@ -0,0 +1,156 @@
+// COMMIT 6 - Quiz Functionality
+
+// COMMIT 6 - Array to store the trivia quizQuestions and answers
+var trivia = [
+  {
+    quizQuestionNumber: "Question 1 of 10",
+    quizQuestion: "Quiz question",
+    quizOptions: ["A", "B", "C", "D"],
+    answer: 0
+  },
+  {
+    quizQuestionNumber: "Question 2 of 10",
+    quizQuestion: "Quiz question",
+    quizOptions: ["A", "B", "C", "D"],
+    answer: 0
+  },
+  {
+    quizQuestionNumber: "Question 3 of 10",
+    quizQuestion: "Quiz question",
+    quizOptions: ["A", "B", "C", "D"],
+    answer: 0
+  },
+  {
+    quizQuestionNumber: "Question 4 of 10",
+    quizQuestion: "Quiz question",
+    quizOptions: ["A", "B", "C", "D"],
+    answer: 0
+  },
+  {
+    quizQuestionNumber: "Question 5 of 10",
+    quizQuestion: "Quiz question",
+    quizOptions: ["A", "B", "C", "D"],
+    answer: 0
+  },
+  {
+    quizQuestionNumber: "Question 6 of 10",
+    quizQuestion: "Quiz question",
+    quizOptions: ["A", "B", "C", "D"],
+    answer: 0
+  },
+  {
+    quizQuestionNumber: "Question 7 of 10",
+    quizQuestion: "Quiz question",
+    quizOptions: ["A", "B", "C", "D"],
+    answer: 0
+  },
+  {
+    quizQuestionNumber: "Question 8 of 10",
+    quizQuestion: "Quiz question",
+    quizOptions: ["A", "B", "C", "D"],
+    answer: 0
+  },
+  {
+    quizQuestionNumber: "Question 9 of 10",
+    quizQuestion: "Quiz question",
+    quizOptions: ["A", "B", "C", "D"],
+    answer: 0
+  },
+  {
+    quizQuestionNumber: "Question 10 of 10",
+    quizQuestion: "Quiz question",
+    quizOptions: ["A", "B", "C", "D"],
+    answer: 0
+  }
+];
+
+// COMMIT 6 - Variables
+
+var currentQuestion = 0;
+var score = 0;
+
+var quizQuestionNumberElement = document.getElementById("quizQuestionNumber")
+var quizQuestionElement = document.getElementById("quizQuestion");
+var quizOptionsElement = document.getElementById("quizOptions");
+var quizResultElement = document.getElementById("quizResult");
+var quizNextButton = document.getElementById("quizNextButton");
+
+// COMMIT 6 - Quiz panel UI
+
+function loadQuestion() {
+  var q = trivia[currentQuestion];
+  quizQuestionNumberElement.textContent = q.quizQuestionNumber;
+  quizQuestionElement.textContent = q.quizQuestion;
+
+  quizOptionsElement.innerHTML = "";
+  for (var i = 0; i < q.quizOptions.length; i++) {
+    var quizButton = document.createElement("quizButton");
+    quizButton.textContent = q.quizOptions[i];
+    quizButton.addEventListener("click", checkAnswer);
+    quizOptionsElement.appendChild(quizButton);
+  }
+
+  if (currentQuestion === trivia.length - 1) {
+    quizNextButton.textContent = "Finish";
+  }
+}
+
+// COMMIT 6 - Checks if answer is correct or incorrect
+
+function checkAnswer(event) {
+  var selectedOption = event.target;
+  var answerIndex = trivia[currentQuestion].answer;
+
+  if (selectedOption.textContent === trivia[currentQuestion].quizOptions[answerIndex]) {
+    score++;
+    quizResultElement.textContent = "Correct!";
+    selectedOption.classList.add("correct");
+  } else {
+    quizResultElement.textContent = "Incorrect!";
+    selectedOption.classList.add("incorrect");
+  }
+
+  // COMMIT 6 - Disable all buttons after answering
+  var buttons = quizOptionsElement.getElementsByTagName("button");
+  for (var i = 0; i < buttons.length; i++) {
+    buttons[i].disabled = true;
+    if (i === answerIndex) {
+      buttons[i].classList.add("correct");
+    }
+  }
+
+  currentQuestion++;
+
+  if (currentQuestion < trivia.length) {
+    quizNextButton.disabled = false;
+  } else {
+    showResults();
+  }
+}
+
+// COMMIT 6 - Shows the results after completing quiz.
+
+function showResults() {
+  quizQuestionElement.style.display = "none";
+  quizOptionsElement.style.display = "none";
+  quizNextButton.style.display = "none";
+
+  quizResultElement.textContent = "You scored " + score + " out of " + trivia.length + " quizQuestions.";
+}
+
+function nextQuestion() {
+  loadQuestion();
+  quizNextButton.disabled = true;
+
+  // COMMIT 6 - Enable buttons for the new quizQuestion
+  var buttons = quizOptionsElement.getElementsByTagName("quizButton");
+  for (var i = 0; i < buttons.length; i++) {
+    buttons[i].disabled = false;
+    buttons[i].classList.remove("correct", "incorrect");
+  }
+
+  quizResultElement.textContent = "";
+}
+
+loadQuestion();
+quizNextButton.addEventListener("click", nextQuestion);
